916e6c47fd22c8f865832d3bd27a0f2cb2240cfe
Parabeac-Nest <admin@parabeac.com>
2022-12-15 17:46:04 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/pomowoko_text_styles.g.dart
@@ -0,0 +1,86 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class PomowokoTextStyles {
+/// Previous style:
+H4/bold
+static const TextStyle h4Bold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+H5/bold
+static const TextStyle h5Bold = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyMediumMedium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/large/semibold
+static const TextStyle bodyLargeSemibold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/large/regular
+static const TextStyle bodyLargeRegular = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+body/large/bold
+static const TextStyle bodyLargeBold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Previous style:
+H2/bold
+static const TextStyle h2Bold = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
